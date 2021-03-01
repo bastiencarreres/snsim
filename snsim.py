933b59ea66014baada13d0558bac86585604b2fa
bcarreres <carrerescppm.in2p.fr>
2021-03-01 17:32:21 +0100

    Some funtion no more in class

--- snsim.py
+++ snsim.py
@@ -13,11 +13,78 @@ import time
 
 c_light_kms = cst.c.to('km/s').value
 snc_mag_offset = 10.5020699 #just an offset -> set_peakmag(mb=0,'bessellb', 'ab') -> offset=2.5*log10(get_x0) change with magsys
+
+def x0_to_mB(par,inv): #Faire 2 fonctions
+    if inv == 0:
+        return -2.5*np.log10(par)+snc_mag_offset
+    else:
+        return pw(10,-0.4*(par-snc_mag_offset))
+
 def box_output(sep,line):
     l = len(sep)-len(line)-2
     space1 = ' '*(l//2)
     space2 = ' '*(l//2+l%2)
     return '#'+space1+line+space2+'#'
+
+def snc_fit(lc,model):
+    return snc.fit_lc(lc, model, ['t0', 'x0', 'x1', 'c'])
+
+def plot_lc(flux_table,bands,zp=25.,mag=False,model=None):
+
+    flux_norm, fluxerr_norm = norm_flux(flux_table,zp)
+    time = flux_table['time']
+
+    t0= flux_table.meta['t0']
+    z = flux_table.meta['z']
+    x0 = flux_table.meta['x0']
+    mb = x0_to_mB(flux_table.meta['x0'],0)
+    x1 = flux_table.meta['x1']
+    c = flux_table.meta['c']
+
+    if model is not None:
+        time_th = np.linspace(t0-20, t0+30,100)
+        model.set(z=z, c=c, t0=t0, x0=x0, x1=x1)
+
+    title = f'$m_B$ = {mb:.3f} $x_1$ = {x1:.3f} $c$ = {c:.4f}'
+
+    plt.figure()
+    plt.title(title)
+    plt.xlabel('Time to peak')
+
+
+    for b in bands:
+        band_mask = flux_table['band']==b
+        flux_b = flux_norm[band_mask]
+        fluxerr_b = fluxerr_norm[band_mask]
+        time_b = time[band_mask]
+        if mag:
+            plt.gca().invert_yaxis()
+            plt.ylabel('Mag')
+            flux_b = flux_b[flux_b>0] #Delete < 0 pts
+            time_b=time_b[flux_b>0]
+            plot = -2.5*np.log10(flux_b)+zp
+            err = 2.5/np.log(10)*1/flux_b*fluxerr_b
+            if model is not None:
+                plot_th = model.bandmag(b,'ab',time_th)
+        else:
+            plt.ylabel('Flux')
+            plot = flux_b
+            err = fluxerr_b
+            if model is not None:
+                plot_th=model.bandflux(b,time_th,zp=zp,zpsys='ab')
+        p = plt.errorbar(time_b-t0,plot,yerr=err,label=b,fmt='o')
+        if model is not None:
+            plt.plot(time_th-t0,plot_th, color=p[0].get_color())
+    plt.legend()
+    plt.show()
+    return
+
+def norm_flux(flux_table,zp):
+    '''Taken from sncosmo -> set the flux to the same zero-point'''
+    norm_factor = pw(10,0.4*(zp-flux_table['zp']))
+    flux_norm=flux_table['flux']*norm_factor
+    fluxerr_norm = flux_table['fluxerr']*norm_factor
+    return flux_norm,fluxerr_norm
 
 class sn_sim :
     def __init__(self,sim_yaml):
@@ -220,82 +287,29 @@ class sn_sim :
         self.sim_mu = 5*np.log10((1+self.zcos)*(1+self.z2cmb)*pw((1+self.zpec),2)*self.cosmo.comoving_distance(self.zcos).value)+25
         #Compute mB : { mu + M0 : the standard magnitude} + {-alpha*x1 + beta*c : scattering due to color and stretch} + {intrinsic smearing}
         self.sim_mB = self.sim_mu + self.M0 - self.alpha*self.sim_x1 + self.beta*self.sim_c + self.mag_smear
-        self.sim_x0 = self.x0_to_mB(self.sim_mB,1)
+        self.sim_x0 = x0_to_mB(self.sim_mB,1)
         return
 
     def gen_flux(self):
         ''' Generate simulated flux '''
-        self.sim_flux=[snc.realize_lcs(obs, self.model, [params],scatter=False)[0] for obs,params in zip(self.obs,self.params)]
+        self.sim_lc=[snc.realize_lcs(obs, self.model, [params],scatter=False)[0] for obs,params in zip(self.obs,self.params)]
         return
 
     def plot_simlc(self,lc_id,zp=25.,mag=False):
-        '''Plot the lc_id lightcurve
-           Use mag=True to plot magnitude'''
-        sim_flux = self.sim_flux[lc_id]
-        z = sim_flux.meta['z']
-        x0 = sim_flux.meta['x0']
-        x1 = sim_flux.meta['x1']
-        c = sim_flux.meta['c']
-        t0 = sim_flux.meta['t0']
-        mb = self.x0_to_mB(x0,0)
-
-        sim_flux_norm, sim_fluxerr_norm, time = self.norm_flux(sim_flux,zp)
-        title = f'$m_B$ = {mb:.3f} $x_1$ = {x1:.3f} $c$ = {c:.4f}'
-
-        self.model.set(z=z, c=c, t0=t0, x0=x0, x1=x1)
-        time_th = np.linspace(t0-20, t0+30,100)
-
-        plt.figure()
-        plt.title(title)
-        plt.xlabel('Time to peak')
-        for b in self.bands:
-            band_mask = sim_flux['band']==b
-            sim_flux_b = sim_flux_norm[band_mask]
-            sim_fluxerr_b = sim_fluxerr_norm[band_mask]
-            time_b = time[band_mask]
-            if mag:
-                plt.gca().invert_yaxis()
-                plt.ylabel('Mag')
-                sim_flux_b = sim_flux_b[sim_flux_b>0] #Delete < 0 pts
-                time_b=time_b[sim_flux_b>0]
-                plot = -2.5*np.log10(sim_flux_b)+zp
-                err = 2.5/np.log(10)*1/sim_flux_b*sim_fluxerr_b
-                plot_th = self.model.bandmag(b,'ab',time_th)
-            else:
-                plt.ylabel('Flux')
-                plot = sim_flux_b
-                err = sim_fluxerr_b
-                plot_th=self.model.bandflux(b,time_th,zp=zp,zpsys='ab')
-            p = plt.errorbar(time_b-t0,plot,yerr=err,label=b,fmt='o')
-            plt.plot(time_th-t0,plot_th, color=p[0].get_color())
-        plt.legend()
-        plt.show()
+        plot_lc(self.sim_lc[lc_id],['ztfg','ztfr','ztfi'],zp=zp,mag=mag,model=self.model)
         return
 
-    def norm_flux(self,flux_table,zp):
-        '''Taken on sncosmo -> set the flux to the same zero-point'''
-        norm_factor = pw(10,0.4*(zp-flux_table['zp']))
-        flux_norm=flux_table['flux']*norm_factor
-        fluxerr_norm = flux_table['fluxerr']*norm_factor
-        return flux_norm,fluxerr_norm,flux_table['time']
-
-    def x0_to_mB(self,par,inv): #Faire 2 fonctions
-        if inv == 0:
-            return -2.5*np.log10(par)+snc_mag_offset
-        else:
-            return pw(10,-0.4*(par-snc_mag_offset))
-
     def fit_lcs(self):
-        self.fit_results = []
+        self.fit_res = []
         for i in range(self.n_sn):
-            self.model.set(z=self.sim_flux[i].meta['z'])  # set the model's redshift.
-            self.fit_results.append(snc.fit_lc(self.sim_flux[i], self.model, ['t0', 'x0', 'x1', 'c']))
+            self.model.set(z=self.sim_lc[i].meta['z'])  # set the model's redshift.
+            self.fit_res.append(snc_fit(self.sim_lc[i],self.model))
         return
 
     def write_sim(self):
         '''Write the simulated lc in a fits file'''
         lc_hdu_list = []
-        for i,tab in enumerate(self.sim_flux):
+        for i,tab in enumerate(self.sim_lc):
             tab.meta['vpec'] = self.vpec[i]
             tab.meta['zcos'] = self.zcos[i]
             tab.meta['zpec'] = self.zpec[i]
@@ -307,4 +321,26 @@ class sn_sim :
 
         hdu_list = fits.HDUList([fits.PrimaryHDU(header=fits.Header({'n_obs': self.n_sn}))]+lc_hdu_list)
         hdu_list.writeto(self.write_path+self.sim_name+'.fits',overwrite=True)
+        return
+
+class open_sim:
+    def __init__(self,sim_file,SALT2_dir):
+        self.salt2_dir = SALT2_dir
+        source = snc.SALT2Source(modeldir=self.salt2_dir)
+        self.model=snc.Model(source=source)
+
+        self.lc_data=[]
+        self.lc_meta=[]
+        with fits.open(sim_file) as sf:
+            self.n_sn=sf[0].header['N_OBS']
+            for hdu in sf[1:]:
+                self.lc_data.append(hdu.data)
+                self.lc_meta.append(hdu.header)
+        return
+
+    def fit_lc(self):
+        self.fit_res=[]
+        for i in range(self.n_sn):
+            self.model.set(z=self.lc_meta[i]['z'])  # set the model's redshift.
+            self.fit_res.append(snc_fit(self.lc_data[i],self.model))
         return
