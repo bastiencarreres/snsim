60ad2cc231a7cd2703adeabe21c9b7a43dc3480c
bcarreres <carreres@cppm.in2p3.fr>
2021-03-16 14:18:46 +0100

    Debug write fit

--- snsim.py
+++ snsim.py
@@ -581,8 +581,8 @@ class sn_sim :
 
     def write_fit(self):
         add_keys = ['t0','e_t0','x0','e_x0','mb','e_mb','x1',\
-                    'e_x1','c','e_c', 'cov_x0_x1','cov_x0_x1',\
-                    'cov_x0_c','cov_mb_x1','cov_mb_c','cov_x1_c',\
+                    'e_x1','c','e_c', 'cov_x0_x1','cov_x0_c',\
+                    'cov_mb_x1','cov_mb_c','cov_x1_c',\
                     'chi2','ndof']
 
         data = {'id' : np.arange(self.n_sn),
@@ -616,7 +616,7 @@ class sn_sim :
 
                 data['c'].append(par[4])
                 data['e_c'].append(np.sqrt(par_cov[2,2]))
-
+                print("cov_x0_x1",len(data['cov_x0_x1']))
                 data['cov_x0_x1'].append(par_cov[0,1])
                 data['cov_x0_c'].append(par_cov[0,2])
                 data['cov_x1_c'].append(par_cov[1,2])
@@ -630,8 +630,8 @@ class sn_sim :
                 for k in add_keys:
                     data[k].append('NaN')
 
-
-
+        for k in data:
+            print(k,len(data[k]))
         table = Table(data)
 
         hdu = fits.table_to_hdu(table)
