a92c9fefef614229a15de538ccbacdb8e1816d4e
bcarreres <carrerescppm.in2p.fr>
2021-05-02 21:54:09 +0200

    begin change on launch_sim

--- scripts/launch_sim.py
+++ scripts/launch_sim.py
@@ -35,7 +35,8 @@ keys_dic = {
         'z_range',
         'v_cmb',
         'M0',
-        'mag_smear'],
+        'mag_smear',
+        'smear_mod'],
     'cosmology': [
         'Om',
         'H0'],
@@ -52,41 +53,6 @@ keys_dic = {
         'host_file',
         'mean_vpec',
         'sig_vpec']}
-type_dic = {'write_path': str,
-            'sim_name': str,
-            'band_dic': dict,
-            'obs_cfg_path': str,
-            'write_format': list,
-            'dbfile_path': str,
-            'db_cut': dict,
-            'zp': float,
-            'gain': float,
-            'ra_size': float,
-            'dec_size': float,
-            'randseed': int,
-            'n_sn': int,
-            'sn_rate': float,
-            'rate_pw': float,
-            'duration': float,
-            'z_range': list,
-            'v_cmb': float,
-            'M0': float,
-            'mag_smear': float,
-	    'smear_mod': str,
-            'Om': float,
-            'H0': float,
-            'salt_dir': str,
-            'version': int,
-            'alpha': float,
-            'beta': float,
-            'mean_x1': float,
-            'mean_c': float,
-            'sig_x1': float,
-            'sig_c': float,
-            'host_file': str,
-            'mean_vpec': float,
-            'sig_vpec': float,
-            'mean_x1': float}
 
 ignore_keys = ['config_path', 'fit']
 
@@ -101,28 +67,19 @@ args = parser.parse_args()
 
 for arg in args.__dict__:
     if arg not in keys_list and arg not in ignore_keys:
-        print(f"{arg} option doesn't exist, arg ignored")
-
+        raise ValueError(f"{arg} option doesn't exist")
+print(args)
 param_dic = {}
 
 with open(args.config_path, "r") as f:
     yml_config = yaml.safe_load(f)
 
 for K in keys_dic:
+    if K not in yml_config:
+        param_dic[K]={}
     for k in keys_dic[K]:
         if args.__dict__[k] is not None:
-            if K not in param_dic:
-                param_dic[K] = {}
-            if k == 'nep_cut':
-                try:
-                    int(k)
-                    type_dic[k] = int
-                except BaseException:
-                    type_dic[k] = list
-            if type_dic[k] == list:
-                param_dic[K][k] = ast.literal_eval(args.__dict__[k])
-            else:
-                param_dic[K][k] = type_dic[k](args.__dict__[k])
+            param_dic[K][k] = args
         elif K in yml_config:
             if K not in param_dic:
                 param_dic[K] = {}
@@ -138,8 +95,9 @@ for K in param_dic:
 
 param_dic['yaml_path'] = args.__dict__['config_path']
 
-sim = snsim.sn_sim(param_dic)
-sim.simulate()
+print(param_dic)
+#sim = snsim.sn_sim(param_dic)
+#sim.simulate()
 
 if args.fit:
     sim.fit_lc()
