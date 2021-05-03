846e7c40b37b54ba1d6e0a7ae98469fc2a53937a
bcarreres <carreres@cppm.in2p3.fr>
2021-05-03 11:38:31 +0200

    New launch script + bug correction on selec ra dec

--- scripts/launch_sim.py
+++ scripts/launch_sim.py
@@ -3,6 +3,15 @@ import yaml
 import ast
 import snsim
 
+def nep_cut(nepc):
+    for i in range(len(nepc)):
+        if len(nepc[i])>=1:
+            nepc[i][0]=int(nepc[i][0])
+        if len(nepc[i])>=3:
+            nepc[i][1]=float(nepc[i][1])
+            nepc[i][2]=float(nepc[i][2])
+    return nepc
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "config_path",
@@ -42,7 +51,7 @@ keys_dic = {
         'H0'],
     'salt_gen': [
         'salt_dir',
-	'version',
+	    'version',
         'alpha',
         'beta',
         'mean_x1',
@@ -54,37 +63,66 @@ keys_dic = {
         'mean_vpec',
         'sig_vpec']}
 
-ignore_keys = ['config_path', 'fit']
+parser.add_argument("--write_path",type=str)
+parser.add_argument("--sim_name",type=str)
+parser.add_argument("--band_dic",type=yaml.load)
+parser.add_argument("--write_format", type=str,nargs='+')
+parser.add_argument("--obs_config_path",type=str)
 
-keys_list = []
-for K in keys_dic:
-    keys_list += keys_dic[K]
+parser.add_argument("--dbfile_path",type=str)
+parser.add_argument("--db_cut",type=yaml.load)
+parser.add_argument("--zp",type=float)
+parser.add_argument("--gain",type=float)
+parser.add_argument("--ra_size",type=float)
+parser.add_argument("--dec_size",type=float)
 
-for k in keys_list:
-    parser.add_argument(f"--{k}")
+parser.add_argument("--randseed",type=int)
+parser.add_argument("--n_sn",type=int)
+parser.add_argument("--sn_rate",type=float)
+parser.add_argument("--rate_pw",type=float)
+parser.add_argument("--duration",type=float)
+
+parser.add_argument("--nep_cut", action='append', nargs='+')
+parser.add_argument("--z_range",type=float,nargs=2)
+parser.add_argument("--v_cmb",type=float)
+parser.add_argument("--M0",type=float)
+parser.add_argument("--mag_smear",type=float)
+parser.add_argument("--smear_mod",type=str)
+
+parser.add_argument("--Om",type=float)
+parser.add_argument("--H0",type=float)
+
+parser.add_argument("--salt_dir",type=str)
+parser.add_argument("--version",type=int)
+parser.add_argument("--alpha",type=float)
+parser.add_argument("--beta",type=float)
+parser.add_argument("--mean_x1",type=float)
+parser.add_argument("--sig_x1",type=float)
+parser.add_argument("--mean_c",type=float)
+parser.add_argument("--sig_c",type=float)
+
+parser.add_argument("--host_file",type=str)
+parser.add_argument("--mean_vpec",type=float)
+parser.add_argument("--sig_vpec",type=float)
 
 args = parser.parse_args()
 
-for arg in args.__dict__:
-    if arg not in keys_list and arg not in ignore_keys:
-        raise ValueError(f"{arg} option doesn't exist")
-print(args)
+if args.nep_cut is not None:
+    args.nep_cut = nep_cut(args.nep_cut)
+
 param_dic = {}
 
 with open(args.config_path, "r") as f:
     yml_config = yaml.safe_load(f)
 
+
 for K in keys_dic:
-    if K not in yml_config:
-        param_dic[K]={}
+    param_dic[K]={}
     for k in keys_dic[K]:
         if args.__dict__[k] is not None:
-            param_dic[K][k] = args
-        elif K in yml_config:
-            if K not in param_dic:
-                param_dic[K] = {}
-            if k in yml_config[K]:
-                param_dic[K][k] = yml_config[K][k]
+            param_dic[K][k] = args.__dict__[k]
+        elif k in yml_config[K]:
+            param_dic[K][k] = yml_config[K][k]
 
 print('Parameters used :\n')
 indent = '    '
@@ -96,8 +134,8 @@ for K in param_dic:
 param_dic['yaml_path'] = args.__dict__['config_path']
 
 print(param_dic)
-#sim = snsim.sn_sim(param_dic)
-#sim.simulate()
+sim = snsim.sn_sim(param_dic)
+sim.simulate()
 
 if args.fit:
     sim.fit_lc()
