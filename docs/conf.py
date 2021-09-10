6700b446f950cde2efe7ae9e702d6d8f95bee237
bcarreres <64126910+bcarreres@users.noreply.github.com>
2021-09-10 17:20:30 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
