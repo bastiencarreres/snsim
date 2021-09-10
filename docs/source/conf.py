b06a46e18b06cd76524861b5dbad32270bc56d49
bcarreres <carreres@cppm.in2p3.fr>
2021-09-10 15:30:29 +0200

    update doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ release = '0.3.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'numpydoc' ]
+extensions = ['recommonmark', 'numpydoc' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,5 +56,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-
