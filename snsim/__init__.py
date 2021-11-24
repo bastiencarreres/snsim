b219f239bf08d6d6c74126bc29e62714ef3f4f53
bcarreres <64126910+bcarreres@users.noreply.github.com>
2021-11-24 09:25:12 +0100

    Update __init__.py

--- snsim/__init__.py
+++ snsim/__init__.py
@@ -11,7 +11,7 @@ Github repository : https://github.com/bcarreres/snsim
 import os
 __snsim_dir_path__ = os.path.dirname(__file__)
 
-__version__ = "0.3.13_dev"
+__version__ = "0.3.13"
 
 from .simu import Simulator
 from .sn_sample import SNSimSample
